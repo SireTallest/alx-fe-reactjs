4a5638a5997922f9c982950def83470012d57927
Abdulbaki <abdoladepo@gmail.com>
2026-01-10 23:09:40 +0100

    Feat: add Header, MainContent, and Footer components :Create Header.jsx component, create MainContent.jsx component, Create Footer.jsx component; Integrate all components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import Footer from './Footer';
+import Main from './MainContent';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -10,6 +14,9 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <Main />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
