4d5508fe84d6f8e0c4b0709921d9131c77c61071
Abdulbaki <abdoladepo@gmail.com>
2026-01-10 23:32:32 +0100

    feat: add UserProfile component: Create UserProfile.jsx component, Import and render in App.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ const UserProfile = (props) =>{
         <div>
             <h2>{props.name}</h2>
             <p>Age: {props.age}</p>
-            <p>Bio: {props.Bio}</p>
+            <p>bio: {props.bio}</p>
         </div>
     );
 }
