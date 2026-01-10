f315f92bc9bf20bb8757e03a4ed0db094d3112f6
Abdulbaki <abdoladepo@gmail.com>
2026-01-10 23:30:33 +0100

    feat: add UserProfile component: Create UserProfile.jsx component, Import and render in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,6 +17,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile  name = "Abdulbaki" age = "27" Bio = "I am a software engineer"/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
