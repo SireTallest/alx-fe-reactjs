47bdcbd24e78fe75ed9319eff7148a5ce3f9caaa
Abdulbaki <abdoladepo@gmail.com>
2026-01-19 14:45:15 +0100

    feat: styling existing UserProfile component

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p style = {{ textAlign: 'left',fontWeight: 'bold' }}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
