49978805f455aa2893ec2aee2eeb146ca1744c2c
Abdulbaki <abdoladepo@gmail.com>
2026-02-01 21:07:28 +0100

    feat: implement user search with loading and error states

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,12 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search';
+import {fetchUserData} from './services/githubService';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [count, setCount] = useState(0)
+  const handleSearch = async (username) =>{
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
+    <div className = "App">
+      <h1>GitHub User Finder</h1>
+      <Search onSearch = {handleSearch}/>
+
+      {loading && <p>Loading...</p>}
+      {error && < p>Looks like we can't find the user, img</p>}
+      {userData && (
+        <div className ="user-info">
+          <img src={userData.avatar_url} alt={userData.name} />
+          <h2>{userData.name || userData.login}</h2>
+          <p>@{userData.login}</p>
+          <a href = {userData.html_url} target="_blank" rel="noopener noreferrer">View Github Profile</a>
+        </div>
+      )}
+    </div>
+      <div>
+        <h1>Github User Finder</h1>
+        <Search onSearch = {handleSearch}/>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +65,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
