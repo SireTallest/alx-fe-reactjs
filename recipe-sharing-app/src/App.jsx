35d656eb0150b5d6ceec9783521f6a985d96b7d2
Abdulbaki <abdoladepo@gmail.com>
2026-02-18 10:37:35 +0100

    feat: integrate recipe components with routing and navigation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,25 @@
-import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
-import FavoritesList from "./components/FavoritesList";
-import RecommendationsList from "./components/RecommendationsList";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-8">
-
-      <h1 className="text-3xl font-bold text-center">
-        Recipe Sharing App
-      </h1>
-
-      <AddRecipeForm />
-
-      <RecipeList />
-
-      <FavoritesList />
-
-      <RecommendationsList />
-
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
