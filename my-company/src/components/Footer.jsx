7ce14576e684762be8fca431e84b4c56ae0c710a
Abdulbaki <abdoladepo@gmail.com>
2026-02-18 12:09:32 +0100

    feat: add Footer component with company information

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div style={{ backgroundColor: 'white', color: 'black', width: '100vw', padding: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+            <p style={{color: 'navy', fontStyle: 'italic', fontWeight: '500'}}>&copy; 2026 My New Company</p>
+        </div>
+    );
+}
+
+export default Footer;
