d3d8db1e7beac8a9eed93042e312c01e382b9a46
Abdulbaki <abdoladepo@gmail.com>
2026-02-18 12:05:12 +0100

    feat: add Navbar component with navigation links

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,4 +1,16 @@
-import { link }  from 'react-router-dom';
-    return(
-        
-    )
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav style={{ backgroundColor: 'white', color: 'black', width: '100vw', padding: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+            <ul style={{ listStyleType: 'none', display: 'flex', gap: '20px'}}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/services">Services</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
