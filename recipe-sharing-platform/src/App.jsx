b8b95de361b14f086a0ea11a961efe899a6b5be4
Abdulbaki <abdoladepo@gmail.com>
2026-02-20 16:56:02 +0100

    feat: implementing HomePage om the App.jsx file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,18 @@
-import { useState } from 'react'
-import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
-export default function App() {
-  return <HomePage />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
